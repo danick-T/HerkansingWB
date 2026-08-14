0464469a7df11d6dbfb727ce1bcb73520c5ffcf6
danick-T <danick.tchang@student.odisee.be>
2026-08-14 21:29:23 +0200

    axios -i

--- projectapplicatieherkansing/src/main.js
+++ projectapplicatieherkansing/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
 
 import { IonicVue } from '@ionic/vue';
 
@@ -36,7 +39,8 @@ import './theme/variables.css';
 
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
+  .use(router)
+  .use(VueAxios, axios);
 
 router.isReady().then(() => {
   app.mount('#app');
